7921724e853d6f34ace9446869673317f075de99
SobakaSlava <nikita_solo_11@mail.ru>
2018-04-11 14:28:36 +0300

    Implement a simple meural network

---
+++ app.js
@@ -0,0 +1,23 @@
+const brain = require('brain.js');
+const trainingData = require('./trainingdata');
+
+const encode = arg => arg.split('').map(x => (x.charCodeAt(0) / 255));
+
+processTrainingData = data => data.map(d => {
+    return {
+      input: encode(d.input),
+      output: d.output
+    }
+  });
+
+const network = new brain.NeuralNetwork();
+
+network.train(processTrainingData(trainingData));
+
+const message = "As a matter conference Democrats House";
+
+const name = brain.likely(encode(message), network);
+
+console.log('that is', name);
+
+console.log(network.run(encode(message)));
---
+++ trainingdata.js
@@ -0,0 +1,48 @@
+const trainingData = [
+  {
+    input: "Inside Chi's nursery",
+    output: { kardashian: 1 }
+  },{
+    input: "Why I dyed my hair pink",
+    output: { kardashian: 1 }
+  },{
+    input: "Feeling Blue (wearing @kkwbeauty powder contour in medium & dark contour kit as eye shadow, & a new lip coming soon)",
+    output: { kardashian: 1 }
+  },{
+    input: "I will be interviewed by @JudgeJeanine on @FoxNews at 9:00 P.M. Enjoy!",
+    output: { trump: 1 }
+  },{
+    input: "Dem Memo: FBI did not disclose who the clients were - the Clinton Campaign and the DNC. Wow!",
+    output: { trump: 1 }
+  },{
+    input: "Thank you to the great men and women of the United States @SecretService for a job well done!",
+    output: { trump: 1 }
+  },{
+    input: "As a matter of National Security I've signed the Omnibus Spending Bill. I say to Congress: I will NEVER sign another bill like this again. To prevent this omnibus situation from ever happening again, I'm calling on Congress to give me a line-item veto for all govt spending bills!",
+    output: { trump: 1 }
+  },{
+    input: "Obama Administration legalized bump stocks. BAD IDEA. As I promised, today the Department of Justice will issue the rule banning BUMP STOCKS with a mandated comment period. We will BAN all devices that turn legal weapons into illegal machine guns.",
+    output: { trump: 1 }
+  },{
+    input: "News conference at the White House concerning the Omnibus Spending Bill.  1:00 P.M.",
+    output: { trump: 1 }
+  },{
+    input: "I am considering a VETO of the Omnibus Spending Bill based on the fact that the 800,000 plus DACA recipients have been totally abandoned by the Democrats (not even mentioned in Bill) and the BORDER WALL, which is desperately needed for our National Defense, is not fully funded.",
+    output: { trump: 1 }
+  },{
+    input: "DACA was abandoned by the Democrats. Very unfair to them! Would have been tied to desperately needed Wall.",
+    output: { trump: 1 }
+  },{
+    input: "House Intelligence Committee votes to release final report. FINDINGS: (1) No evidence provided of Collusion between Trump Campaign & Russia. (2)  The Obama Administrations Post election response was insufficient. (3) Clapper provided inconsistent testimony on media contacts.",
+    output: { trump: 1 }
+  },{
+    input: "Can’t wait for you to try it!!",
+    output: { kardashian: 1 }
+  },{
+    input: "We also restocked Crystal Gardenia Citrus on http://KKWFRAGRANCE.COM  25% off today only for #nationalfragranceday",
+    output: { kardashian: 1 }
+  }
+];
+
+
+module.exports = trainingData;
